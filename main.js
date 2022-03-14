230cb72b59b9bb648e95499c28f20ecf04368230
Bruno <brunocostaprado@gmail.com>
2022-03-14 02:49:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,63 +1,29 @@
 const Discord = require("discord.js");
+const { Client, Intents } = require("discord.js");
+// const client = new Discord.Client();
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-const client = new Discord.client();
+const prefix = "-";
 
-client.once("read", () => {
+client.once("ready", () => {
   console.log("Funciona");
 });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
-client.console.log(
-  "OTUyNDQ0NjM5MDc4NjU4MDY5.Yi2HMQ.5JB7NUesIzpTAz-zjs5v-Qs8vEU"
-);
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split("/ + /");
+  const command = args.shift().toLocaleLowerCase();
+
+  if (command === "ping") {
+    message.channel.send("pong!");
+  } else if (command == "youtube") {
+    message.channel.send("https://www.youtube.com/MynameisByf");
+  }else if(command=="Father","Dad","Creator","father","dad","creater"){
+    message.channel.send("I was create by Bruno Costa Prado, you can find his repo herer: https://github.com/BrunoCostaPrado?tab=repositories")
+  }
+});
+
+client.login("Auth_Token");
